7a1b69dffe6d3925719dc079e86f00010ddc0823
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-03 20:16:22 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,33 @@
-import React from 'react';
-const Todo = () => {
-    return (
-        <></>
-    )
-}
+import React from "react";
+
+const Todo = ({ todo, dispatch }) => {
+  const handleDelete = () => {
+    dispatch({
+      type: "DELETE_TODO",
+      payload: todo.id,
+    });
+  };
+
+  return (
+    <div className="todo">
+      <div className="todo-title">{todo.title}</div>
+      <button className="todo-delete" onClick={handleDelete}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export { Todo };
 
 
-export { Todo }
+// import React from 'react';
+// const Todo = () => {
+//     return (
+//         <></>
+//     )
+// }
+
+
+// export { Todo }
 
